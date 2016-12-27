81d1a08a49521bcbcb2fdd7efaa6d9ee18fb386f
Wiggins-zsz <1341378393@qq.com>
2016-12-27 20:04:12 +0800

    format

--- app/components/Lifting.js
+++ app/components/Lifting.js
@@ -7,21 +7,28 @@ const scaleNames = {
 };
 
 function tryConvert(value, convert) {
-  const input = parseFloat(value);
-  if (Number.isNaN(input)) {
-    return '';
-  }
-  const output = convert(input);
-  const rounded = Math.round(output * 1000) / 1000;
-  return rounded.toString();
+	const input = parseFloat(value);
+	if (Number.isNaN(input)) {
+		return '';
+	}
+	const output = convert(input);
+	const rounded = Math.round(output * 1000) / 1000;
+	return rounded.toString();
 }
 
 function toCelsius(fahrenheit) {
-  return (fahrenheit - 32) * 5 / 9;
+	return (fahrenheit - 32) * 5 / 9;
 }
 
 function toFahrenheit(celsius) {
-  return (celsius * 9 / 5) + 32;
+    return (celsius * 9 / 5) + 32;
+}
+
+function BoilingVerdict(props) {
+	if (props.celsius >= 100) {
+		return <p className="tips-w">The water would boil.</p>;
+	}
+	return <p className="tips-s">The water would not boil.</p>;
 }
 
 class TemperatureInput extends React.Component {
@@ -46,15 +53,6 @@ class TemperatureInput extends React.Component {
     );
   }
 }
-
-function BoilingVerdict(props) {
-  if (props.celsius >= 100) {
-    return <p className="tips-w">The water would boil.</p>;
-  }
-  return <p className="tips-s">The water would not boil.</p>;
-}
-
-
 
 class Calculator extends React.Component {
   constructor(props) {
